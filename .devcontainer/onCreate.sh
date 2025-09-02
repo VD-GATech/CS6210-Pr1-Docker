075b4c0bcb41138a0224fac1afdbb8be527cb661
VD-GATech <vdao32@gatech.edu>
2025-09-02 11:41:13 -0400

    Upgrade guess vm to 22.04 (Fix GLIBC bug)

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -15,11 +15,15 @@ ssh-keygen -t rsa -q -f "$HOME/.ssh/id_rsa" -N ""
 
 # Download uvtool + ubuntu image
 apt-get install -y uvtool
-uvt-simplestreams-libvirt sync release=bionic arch=amd64
+
+# Upgrade aos_vm to 22.04
+#uvt-simplestreams-libvirt sync release=bionic arch=amd64
+uvt-simplestreams-libvirt sync release=jammy arch=amd64
 
 # Let it do in background maybe better than cloning.
 for i in {1..8}; do
-	uvt-kvm create aos_vm$i release=bionic --memory=512 --no-start
+	#uvt-kvm create aos_vm$i release=bionic --memory=512 --no-start
+ 	uvt-kvm create aos_vm$i release=jammy --memory=512 --no-start
 done
 
 pkill libvirtd
